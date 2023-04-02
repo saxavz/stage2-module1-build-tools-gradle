dbd62e1c86d466f0deda5af5ffe9a563d29432d4
Aliaksandr Hrakhouski <aliaksandr_hrakhouski@epam.com>
2023-04-02 03:39:33 +0200

    handling nulls and empty stings

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if(str == null || str.length() == 0){
+            return false;
+        }
+
         return Double.parseDouble(str) > 0d;
     }
 }
