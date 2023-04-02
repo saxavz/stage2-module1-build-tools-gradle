ab635266969976981297482cc5a3b4342f4fe034
Aliaksandr Hrakhouski <aliaksandr_hrakhouski@epam.com>
2023-04-02 03:18:28 +0200

    adding tests, and arranging options in gradle configs

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        if(args.size() == 0){
+            return false;
+        }
         return
         args.stream()
                 .allMatch(StringUtils::isPositiveNumber);
---
+++ demo/src/test/java/IsAllPositiveNumbersTest.java
@@ -0,0 +1,44 @@
+import com.epam.demo.Utils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IsAllPositiveNumbersTest {
+
+    public static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of("EmptySourceList",
+                        new ArrayList<>(),
+                        false),
+
+                Arguments.of("Only Positive Nums",
+                        Arrays.asList("47", "3", "25", "13", "7"),
+                        true),
+
+                Arguments.of("Has Negative Num",
+                        Arrays.asList("47", "3", "25", "13", "-7"),
+                        false),
+
+                Arguments.of("All Negative Nums",
+                        Arrays.asList("-47", "-3", "-25", "-13", "-7"),
+                        false)
+        );
+    }
+
+
+
+    @ParameterizedTest(name = "isAllPositiveNumbers_{0}_Test")
+    @MethodSource(value = "testCases")
+    void createArrayListTest(String name,
+                             List<String> actualNumbersList,
+                             boolean expectedValue) {
+        assertEquals(expectedValue, Utils.isAllPositiveNumbers(actualNumbersList));
+    }
+}
